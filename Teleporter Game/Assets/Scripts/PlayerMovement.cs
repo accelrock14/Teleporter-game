8cf4bbab4599eb274b47b8a120fdf47543263002
Jeferson X <jefersonx0140@gmail.com>
2023-02-15 19:32:18 +0530

    Player-Movement

---
+++ Teleporter Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+    public Rigidbody2D weapon;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+}
